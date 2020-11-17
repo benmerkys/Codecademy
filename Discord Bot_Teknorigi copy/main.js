9196fafb56aeb55dcb94da39c7787952da86e28a
Benediktas Merkys <benediktas.merkys@gmail.com>
2020-11-17 01:17:59 +0000

    kurainto-kun

---
+++ Discord Bot_Teknorigi copy/main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('Teknorigi-chan engaged.');
+})
+
+
+client.login('Nzc4MDM1NzU4ODk5OTIwODk2.X7MIRA.FIJyYyZ8zZ-L5XWm1fFZJ5EwsgI');
