c005bdd66c6317b83bc125f9eae49a59452cf301
Benediktas Merkys <benediktas.merkys@gmail.com>
2020-11-17 09:52:11 +0000

    tekonorigi

---
+++ Discord Bot_Tekonorigi/main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('Tekonorigi-chan engaged.');
+})
+
+
+client.login('Nzc4MDM1NzU4ODk5OTIwODk2.X7MIRA.5lNd_XwV77cp2nQ82AGtSfO4re8');
